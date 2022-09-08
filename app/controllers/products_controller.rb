91362297a9cd9c6641f160ec8c9fdf4223ee6359
Rickie Natividad <rickienatividad85@gmail.com>
2022-09-07 23:03:35 -0400

    adds RESTful actions to routes.rb and products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,39 @@
 class ProductsController < ApplicationController
+  def index
+    product = Product.all
+    render json: product.as_json
+  end
+
+  def show
+    product = Product.find_by(id: params["id"])
+    render json: product.as_json
+  end
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["url"],
+      description: params["description"],
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  def update
+    product = Product.find_by(id: params["id"])
+    product.name = params["name"] || product.name
+    product.price = params["price"] || product.price
+    product.image_url = params["image_url"] || product.image_url
+    product.description = params["description"] || product.description
+
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find_by(id: params["id"])
+    product.destroy
+    render json: { message: "Product was destroyed." }
+  end
 end
