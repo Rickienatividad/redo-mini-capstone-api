df29c0e77540a2b05b14c58ea993fd2f68be83b9
Rickie Natividad <rickienatividad85@gmail.com>
2022-09-08 10:55:03 -0400

    adds is_discounted?, tax, and total methods for Product. adds views files (index.json.jb, show.json.jb)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,30 +5,33 @@ class ProductsController < ApplicationController
   end
 
   def show
-    product = Product.find_by(id: params["id"])
-    render json: product.as_json
+    @product = Product.find_by(id: params["id"])
+    # render json: product.as_json(methods: [:is_discounted?, :tax, :total])
+    render template: "products/show"
   end
 
   def create
-    product = Product.new(
+    @product = Product.new(
       name: params["name"],
       price: params["price"],
       image_url: params["url"],
       description: params["description"],
     )
-    product.save
-    render json: product.as_json
+    @product.save
+    # render json: product.as_json
+    render template: "products/show"
   end
 
   def update
-    product = Product.find_by(id: params["id"])
-    product.name = params["name"] || product.name
-    product.price = params["price"] || product.price
-    product.image_url = params["image_url"] || product.image_url
-    product.description = params["description"] || product.description
+    @product = Product.find_by(id: params["id"])
+    @product.name = params["name"] || product.name
+    @product.price = params["price"] || product.price
+    @product.image_url = params["image_url"] || product.image_url
+    @product.description = params["description"] || product.description
 
-    product.save
-    render json: product.as_json
+    @product.save
+    # render json: product.as_json
+    render template: "products/show"
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price < 10
+      return true
+    end
+  end
+
+  def tax
+    return price * 0.09
+  end
+
+  def total
+    return price + tax
+  end
 end
